5c162cf300b8ba6abbdd6c9f0dd45793a6c62968
Azki Rasyid <73322974+azkira@users.noreply.github.com>
2021-04-30 21:35:31 +0700

    benerin dikit

--- phpdasar/register.php
+++ phpdasar/register.php
@@ -5,31 +5,23 @@ include("registercontroller.php");
 // html
 include("header.php");
 ?>
-    <div class="row">
-        <div data-alert class="alert-box" style="display:none;">
-        Ini standard error
-        <a href="#" class="close">&times;</a>
-        </div>
+<div data-alert class="alert-box" style="display:none;">
+    Ini standard error
+    <a href="#" class="close">&times;</a>
+</div>
+<form action="register.php" method="POST">
+    <div class="row column">
+        <h4 class="text-center">Register</h4>
+        <label>Username
+            <input type="text" name="username" placeholder="Username">
+        </label>
+        <label>Password
+            <input type="password" name="password" placeholder="Password">
+        </label>
+        <input id="show-password" type="checkbox"><label for="show-password">Show password</label>
+        <p><input type="submit" name="submit" class="button expanded" value="DAFTAR" /></p>
     </div>
-    <div class="row">
-        <div class="medium-6 medium-centered large-4 large-centered columns">
-            <div class="row log-in-form">
-                <form action="register.php" method="POST">
-                    <div class="row column">
-                        <h4 class="text-center">Register</h4>
-                        <label>Username
-                        <input type="text" name="username" placeholder="Username">
-                        </label>
-                        <label>Password
-                        <input type="password" name="password" placeholder="Password">
-                        </label>
-                        <input id="show-password" type="checkbox"><label for="show-password">Show password</label>
-                        <p><input type="submit" name="submit" class="button expanded" value="DAFTAR"/></p>  
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+</form>
 
 <?php
 include("footer.php");
